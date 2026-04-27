dd9ed45b9b7ba35392ebdd291ba50b5060b8a7d3
PIKLUU <107104749+PIKLUU@users.noreply.github.com>
2026-04-27 18:16:34 +0530

    Update streams.js

--- streams.js
+++ streams.js
@@ -3,9 +3,15 @@
 // To add a new channel, just copy the format and add it to the list!
 const myChannels = [
     { name: "Channel 1", link: "https://drive.uber.radio/uber/bollywoodlove/icecast.audio" },
-    { name: "Channel 2", link: "https://s6.yesstreaming.net/proxy/john1237?mp=/live" },
-    { name: "Channel 3", link: "https://stream-170.zeno.fm/9phrkb1e3v8uv?zt=eyJhbGciOiJIUzI1NiJ9.eyJzdHJlYW0iOiI5cGhya2IxZTN2OHV2IiwiaG9zdCI6InN0cmVhbS0xNzAuemVuby5mbSIsInJ0dGwiOjUsImp0aSI6IjZxOVF3aWNBUURHLVB0WE5CTXVrbGciLCJpYXQiOjE3NzcyMjc5OTcsImV4cCI6MTc3NzIyODA1N30.3jX5QjOV_1wHjEgzXW3DwK_ErjjpBMHowy0fmaKWg8s" },
-    { name: "Channel 4", link: "https://stream-289.zeno.fm/zu59ykebs2zuv?zt=eyJhbGciOiJIUzI1NiJ9.eyJzdHJlYW0iOiJ6dTU5eWtlYnMyenV2IiwiaG9zdCI6InN0cmVhbS0yODkuemVuby5mbSIsInJ0dGwiOjUsImp0aSI6IjJDOUV4MTZjU1RlN29tYjFyWTc3eFEiLCJpYXQiOjE3NzcyMjg1MjksImV4cCI6MTc3NzIyODU4OX0.NW9pQHoMa1pdfmDLf_YMGRK2fYH4qwLVX2WOh2w3rjo" }
+{ name: "Channel 2", link: "https://2.mystreaming.net/uber/bollywoodnow/icecast.audio" },
+{ name: "Channel 3", link: "https://stream-287.zeno.fm/rqqps6cbe3quv?zt=eyJhbGciOiJIUzI1NiJ9.eyJzdHJlYW0iOiJycXFwczZjYmUzcXV2IiwiaG9zdCI6InN0cmVhbS0yODcuemVuby5mbSIsInJ0dGwiOjUsImp0aSI6IklBY2drSS1IVFVLM2JMeVZJV2wyUGciLCJpYXQiOjE3NzcyNjk3NTcsImV4cCI6MTc3NzI2OTgxN30.ExL2lU1wFjSGyaziQs9S-2AfWfltMLbJhagthc-05iA" },
+{ name: "Channel 4", link: "https://stream-289.zeno.fm/zu59ykebs2zuv?zt=eyJhbGciOiJIUzI1NiJ9.eyJzdHJlYW0iOiJ6dTU5eWtlYnMyenV2IiwiaG9zdCI6InN0cmVhbS0yODkuemVuby5mbSIsInJ0dGwiOjUsImp0aSI6IkZiY21BeFpsU0lPcDJtRDEzSzVfcVEiLCJpYXQiOjE3NzcyNjk5ODksImV4cCI6MTc3NzI3MDA0OX0.HDwkFzYjOaThJUGAPrc94KVRd5yJnO7RIDhvBWg_3GA" },
+{ name: "Channel 5", link: "https://stream-289.zeno.fm/143d7gty24zuv?zt=eyJhbGciOiJIUzI1NiJ9.eyJzdHJlYW0iOiIxNDNkN2d0eTI0enV2IiwiaG9zdCI6InN0cmVhbS0yODkuemVuby5mbSIsInJ0dGwiOjUsImp0aSI6IkJTOTFDVlE3U3FDN2tZWWh5elV0VUEiLCJpYXQiOjE3NzcyNjk0NzIsImV4cCI6MTc3NzI2OTUzMn0.Su1D0aTe3zM5pBNCtVqLRkLHBZPjkgM6QHYVk20ebRg" },
+{ name: "Channel 6", link: "https://stream-286.zeno.fm/eyxg23ky4x8uv?zt=eyJhbGciOiJIUzI1NiJ9.eyJzdHJlYW0iOiJleXhnMjNreTR4OHV2IiwiaG9zdCI6InN0cmVhbS0yODYuemVuby5mbSIsInJ0dGwiOjUsImp0aSI6ImFLT0tfMGt5VDlLWWVmeUxYM3JUVFEiLCJpYXQiOjE3NzcyNzExNjMsImV4cCI6MTc3NzI3MTIyM30.aa-1LBSCWtQDaiEFJupVYDgXxLdswjCjVz7d0psEdao" },
+{ name: "Channel 7", link: "https://stream-289.zeno.fm/rm4i9pdex3cuv?zt=eyJhbGciOiJIUzI1NiJ9.eyJzdHJlYW0iOiJybTRpOXBkZXgzY3V2IiwiaG9zdCI6InN0cmVhbS0yODkuemVuby5mbSIsInJ0dGwiOjUsImp0aSI6IlFoTVdEQ3VZUThXMjhRSk5LQlJISlEiLCJpYXQiOjE3NzcyNjkwMTQsImV4cCI6MTc3NzI2OTA3NH0.wVQMqXot8Glqw1M7RZS2xJzAW7ecII4u8ThT-85nccE" },
+{ name: "Channel 8", link: "https://server.mixify.in/listen/listen/radio.mp3" },
+{ name: "Channel 9", link: "https://s6.yesstreaming.net/proxy/john1237?mp=/live" },
+{ name: "Channel 10", link: "https://stream-170.zeno.fm/9phrkb1e3v8uv?zt=eyJhbGciOiJIUzI1NiJ9.eyJzdHJlYW0iOiI5cGhya2IxZTN2OHV2IiwiaG9zdCI6InN0cmVhbS0xNzAuemVuby5mbSIsInJ0dGwiOjUsImp0aSI6InZfbFZJbUg4VElPZndSQzVGMy1KaVEiLCJpYXQiOjE3NzcyNjg5NTgsImV4cCI6MTc3NzI2OTAxOH0.KyXXAzkUDcHDgUGLIabEcWUougRn83sETzV4C0J2dqU" }
 ];
 
 
